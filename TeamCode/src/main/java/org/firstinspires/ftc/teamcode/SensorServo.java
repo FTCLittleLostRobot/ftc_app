cca80a991ab52dac896c1429eb67f216b3ad7bb0
didzi1 <40848231+didzi1@users.noreply.github.com>
2018-11-25 16:08:46 -0800

    made the servo go smaller amounts at a time, and finshed writing state machines for the sampling. Need to move state machines part for sampling into the competion program.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorServo.java
@@ -13,11 +13,6 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 public class SensorServo extends OpMode {
 
     public Servo servo = null;
-    private boolean up = true;
-    private boolean DpadDOWN = false;
-    private boolean crntDpdDOWN = false;
-    private boolean DpadUP = false;
-    private boolean crntDpdUP = false;
 
 
     public void init()
@@ -33,31 +28,18 @@ public class SensorServo extends OpMode {
         telemetry.addData("Servo is at", servo.getPosition());
 
 
-        // while the op mode is active, loop and read the light levels.
-        // Note we use opModeIsActive() as our loop condition because it is an interruptible method.
-        // send the info back to driver station using telemetry function.
-        // if the digital channel returns true it's HIGH and the button is unpressed.
         if (gamepad2.dpad_down) {
-            up = false;
+            servo.setPosition(servo.getPosition() - (double)0.01);
             telemetry.addData("Say", "Servo is going clockwise");
 
         }
 
         if (gamepad2.dpad_up) {
-            up = true;
+            servo.setPosition(servo.getPosition() + (double)0.01);
             telemetry.addData("Say", "Servo is going counter clockwise");
 
         }
 
-        if (up)
-        {
-            servo.setPosition(servo.getPosition() + 0.1);
-
-        }
-        else
-        {
-            servo.setPosition(servo.getPosition() - 0.1);
-        }
     }
 
 }
