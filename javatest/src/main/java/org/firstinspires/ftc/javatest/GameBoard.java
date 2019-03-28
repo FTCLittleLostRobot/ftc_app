192b8f46439e841c563553c3ceb12da74ee6daa5
Wizoi <kidzi+github@idzifamily.com>
2019-03-27 19:36:56 -0700

    adfhghj,k,jhgfd

---
+++ javatest/src/main/java/org/firstinspires/ftc/javatest/GameBoard.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.javatest;
+
+public class GameBoard {
+
+    public void DisplayBoard() {
+    }
+    public boolean IsGameOver(){return true;}
+    public void TicTacTurn(String player, int square){}
+    public void DisplayEndGame(String WinLossTie){}
+}
