3dfe25c0a53b9a09eba48dc70409d9fc4024ad26
CadenTran <43486533+CadenTran@users.noreply.github.com>
2019-03-27 19:44:53 -0700

    Update the project

---
+++ javatest/src/main/java/org/firstinspires/ftc/javatest/TicTacToe.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.javatest;
+
+//import java.util.SplashScreen;
+
+public class TicTacToe {
+    private void DisplayScreen() {
+        System.out.println("Tic Tac Toe! Enter position into textbox! Must be In number ");
+    }
+
+    public void StartGame() {
+        DisplayScreen();
+
+        GameBoard gameboard = new GameBoard();
+        gameboard.DisplayBoard();
+
+
+
+
+
+       //  if (IsGameOver True) {
+
+      //  }
+        //print splash screen
+
+        // loop
+        //display board
+       //player takes turn
+        //IsGameOver
+        //If true display : Winner _ Player
+        //If false continue with game
+    }
+}
